f62f0fc7a7a01ed8c0470d68714d90a969c8931f
Arturo Rinaldi <arty.net2@gmail.com>
2020-04-09 18:15:09 +0200

    fix for newer gcc compilers for -fpermissive strict behaviour
    URL: https://stackoverflow.com/questions/9280921/iso-c-forbids-comparison-between-pointer-and-integer-fpermissive-c

--- src/Command.cpp
+++ src/Command.cpp
@@ -674,7 +674,7 @@ CommandMwb::invoke(char* argv[], int argc)
             char* input = readline("? ");
             if (!input)
                 return;
-            if (input == '\0' ||
+            if (input == "\0" ||
                 !argUint32(input, &value))
             {
                 free(input);
@@ -777,7 +777,7 @@ CommandMww::invoke(char* argv[], int argc)
             char* input = readline("? ");
             if (!input)
                 return;
-            if (input == '\0' ||
+            if (input == "\0" ||
                 !argUint32(input, &value))
             {
                 free(input);
